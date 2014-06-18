c1cc5d1879acca67772b40039d7ae8080662bbc9
Greg Cosgrove <gregcosgrove@gmail.com>
2014-06-18 08:46:49 -0500

    Clean up seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,8 @@ pols.each do |pol|
             nickname: pol.nickname, last_name: pol.lastname,
             name_suffix: pol.name_suffix, party: pol.party, state: pol.state,
             district: pol.district, in_office: pol.in_office, gender: pol.gender, birthdate: pol.birthdate,
-            senate_class: pol.senate_class,
-            bioguide_id: pol.bioguide_id,
-            votesmart_id: pol.votesmart_id, govtrack_id: pol.govtrack_id,
-            crp_id: pol.crp_id}
+            senate_class: pol.senate_class, bioguide_id: pol.bioguide_id, votesmart_id: pol.votesmart_id,
+            govtrack_id: pol.govtrack_id, crp_id: pol.crp_id}
       Politician.create(hash)
 end
 
